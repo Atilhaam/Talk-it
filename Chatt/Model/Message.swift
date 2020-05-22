d9491258e636c8b0c9ea1eaf1d118d4c85451846
Atilhaam <ilham.wayne@gmail.com>
2020-05-23 00:48:42 +0700

    first committ

---
+++ Chatt/Model/Message.swift
@@ -0,0 +1,17 @@
+//
+//  Message.swift
+//  Chatt
+//
+//  Created by Ilham on 12/05/20.
+//  Copyright © 2020 Ilham. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender : String
+    let body : String
+    
+    
+    
+}
---
+++ Podfile
@@ -0,0 +1,9 @@
+ platform :ios, '13.0'
+
+target 'Chatt' do
+  use_frameworks!
+
+  # Pods for Chatt
+  pod 'Firebase/Auth'
+  pod 'Firebase/Firestore'
+end
